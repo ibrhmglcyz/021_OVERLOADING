561ed3eb62e9b3eed6a7b14eaa946ebbc2542850
ibrhmglcyz <ibrhmglcyz@192.168.1.21>
2019-05-21 19:05:19 +0300

    OVERLOADING UYGULAMASI

---
+++ src/com/vektorel/Islemler_Overloading.java
@@ -0,0 +1,12 @@
+package com.vektorel;
+
+public class Islemler_Overloading {
+
+	public void toplam(int a, int b) {}
+	
+	public void toplam(double c, int d) {}
+	
+	public void toplam(double a, float b) {}
+	
+	public void toplam(float a, int b) {}
+}
---
+++ src/com/vektorel/Runner.java
@@ -0,0 +1,14 @@
+package com.vektorel;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Islemler_Overloading islem = new Islemler_Overloading();
+		
+		islem.toplam(125.2, 25);
+		
+
+	}
+
+}
